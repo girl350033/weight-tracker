a2bd3b4b90ed07f2f988106c943883a9c4ba8c5b
girl350033 <girl350033@gmail.com>
2026-08-18 15:59:00 +0800

    Fix formatting and add label visibility to radio

--- app.py
+++ app.py
@@ -385,8 +385,10 @@ if "loaded_food_key" not in st.session_state:
 st.title("健康與體態追蹤系統")
 
 person = st.radio(
+    "選擇使用者",
     ["Shing", "Gloria"],
-    horizontal=True
+    horizontal=True,
+    label_visibility="collapsed"
 )
 
 
